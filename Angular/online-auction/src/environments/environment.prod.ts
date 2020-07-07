e809650c9656a3b337876411ad3607516a6f3f82
Romulo Rocha Lemes <rrlemes1@latam.stefanini.com>
2020-07-06 21:36:20 -0300

    Alteração no caminhos

--- Angular/online-auction/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  stsAuthority: "http://localhost:5101/",
-  clientId: "implicit",
-  clientRoot: "http://localhost:4200/",
-  clientScope: "openid profile email api",
-  apiRoot: "https://demo.identityserver.io/api/",
-};
